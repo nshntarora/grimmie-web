98798d1b8726c57512028fb63840e405e07236a1
Nishant Arora <na.nishantarora@gmail.com>
2016-06-19 03:37:55 +0530

    Fixed JSON response

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ var responseObject = {
 app.get('/',function(req,res){
 	con.query('SELECT * FROM `users` WHERE `fb_id` = ?', ['121'], function (error, results, fields) {
 		console.log(results);
-		res.send(results);
+		res.send(results[0]);
   // error will be an Error if one occurred during the query 
   // results will contain the results of the query 
   // fields will contain information about the returned results fields (if any) 
