131b32046b5276dd2e4e6bb4c4d6e51026449390
Sujay Kundu <sujaykundu777@gmail.com>
2016-06-20 01:47:13 +0530

    Webapp Module Added

---
+++ web-app/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 			"505337193009298",
+	"facebook_api_secret"	:				"21f6b18108a1739b5064f4d25a3b674d",
+	"callback_url"				:				"http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"false",
+	"host"								:				"localhost",
+	"username"						:				"root",
+	"password"						:				"",
+	"database"						:				""
+}
